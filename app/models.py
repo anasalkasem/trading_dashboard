326f52a821242c2014523fff9f96852a7f84b69b
anasalkasem <anass111173@gmail.com>
2025-05-05 03:46:22 -0500

    Delete app directory

--- app/models.py
+++
@@ -1,9 +0,0 @@
-from . import db
-
-class Trade(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    symbol = db.Column(db.String(20))
-    action = db.Column(db.String(10))
-    price = db.Column(db.Float)
-    timestamp = db.Column(db.String(50))
-    reasons = db.Column(db.String(200))
